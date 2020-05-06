3526595e5b79fab9b9ab0f616f37839abeac5f7b
Marcin Czepiela <czepimm200@gmail.com>
2020-05-06 12:08:20 +0200

    Build 1.4

---
+++ precache-manifest.51102ef082cd42848d25596e194b92b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c482c622f685bce063c8fbdf4e55d56",
+    "url": "/Projekt-zespolowy/index.html"
+  },
+  {
+    "revision": "993f53610a3b5a0096bf",
+    "url": "/Projekt-zespolowy/static/css/main.e1f313b0.chunk.css"
+  },
+  {
+    "revision": "b139e1352b3a04e8be7a",
+    "url": "/Projekt-zespolowy/static/js/2.db9a609b.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/Projekt-zespolowy/static/js/2.db9a609b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "993f53610a3b5a0096bf",
+    "url": "/Projekt-zespolowy/static/js/main.7aeb7eb6.chunk.js"
+  },
+  {
+    "revision": "40cabfd6d5b5534e06bb",
+    "url": "/Projekt-zespolowy/static/js/runtime-main.c6f82277.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Projekt-zespolowy/precache-manifest.7c84fcce4ce0f82b5e7810b005952705.js"
+  "/Projekt-zespolowy/precache-manifest.51102ef082cd42848d25596e194b92b9.js"
 );
 
 self.addEventListener('message', (event) => {
