0ebfea76e1f798d99a340fe47cd5c9c8cd0a10e8
Marcin Czepiela <czepimm200@gmail.com>
2020-05-06 11:33:40 +0200

    Build 1.0

---
+++ precache-manifest.6157141a091ef5b8e36e3333d4fd7a06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "106a90e2a659d44cfab52dbebc729b09",
+    "url": "/Projekt-zespolwy-front/index.html"
+  },
+  {
+    "revision": "7812e489d8ac4c7ada46",
+    "url": "/Projekt-zespolwy-front/static/css/main.e1f313b0.chunk.css"
+  },
+  {
+    "revision": "b642b82914607018660e",
+    "url": "/Projekt-zespolwy-front/static/js/2.56359736.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/Projekt-zespolwy-front/static/js/2.56359736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7812e489d8ac4c7ada46",
+    "url": "/Projekt-zespolwy-front/static/js/main.342b8b90.chunk.js"
+  },
+  {
+    "revision": "3e86a72f123e32ead3c6",
+    "url": "/Projekt-zespolwy-front/static/js/runtime-main.caf8be93.js"
+  }
+]);
